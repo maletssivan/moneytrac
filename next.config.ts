8015484f430ea9ff75fe728b07256f508b944ccb
Ivan <Maletssivan@gmail.com>
2026-05-12 21:32:48 +0300

    fix: update next.config for Vercel compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  skipMiddlewareUrlNormalize: true,
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
